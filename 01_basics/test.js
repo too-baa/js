d4b29482f67bd943ca957a480b609465dbf24267
Tooba A <toobaashfaque7@gmail.com>
2026-07-21 18:42:41 +0000

    first code

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+/*
+you may use ; at the end of a line 
+*/
+
+const custId = 78956
+let custName = "tanya"
+var custCity = "Allahabad" // var-> scope prob, not preferred
+password = "fa234" // not recommended too, use let
+let custState
+
+console.log(custName)
+
+// custId=889 #cannot be changed
+
+custName = "shriya"
+custCity = "Kanpur"
+password="hg466"
+console.table([custId, custName, custCity, password, custState])
