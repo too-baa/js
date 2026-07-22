77941982f23178fc3a73cb9b314cd5bb10af4ee1
too_bad_ <toobaashfaque7@gmail.com>
2026-07-22 17:42:42 +0000

    conversion experiments

--- 01_basics/03_conversionofdatatypes.js
+++ 01_basics/03_conversionofdatatypes.js
@@ -1,34 +1,41 @@
 /*
+CONVERSION TO NUMBER
 '76'=> 76, number
 '353abc'=> NaN, number
 'abc'=> NaN, number
 true=> 1; false=0, number
-unddefined=> NaN, number
+undefined=> NaN, number
+null=> 0
 */
 
-let score1 = "7"
+// let score1 = "7"
 // console.log(typeof score1)
 
 // let valueInNumber = Number(score1)
 // console.log(typeof valueInNumber)
 // console.log(valueInNumber)
 
-let score2 = "65abc"
-// let valueInNumber = Number(score2)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
+/*
+CONVERSION TO BOOLEAN
+1 & other numbers=> true; 0=>false
+''=> false
+'ok'=> true
+undefined=> false
+null=> false
+*/
 
-let score3 = 'abh'
-// let valueInNumber = Number(score3)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber)
+// let isLoggedIn = null
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-let score4 = true
-// let valueInNumber = Number(score4)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber) 
-
-let score5 = undefined
-let valueInNumber = Number(score5)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+/*
+CONVERSION TO STRING
+45=> '45'
+true=> 'true'
+undefined=> 'undefined'
+null=> 'null'
+*/
+let str = null
+console.log(typeof str)
+let stringStr = String(str)
+console.log(stringStr)
