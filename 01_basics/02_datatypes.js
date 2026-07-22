32bae6e5e2e57cdfb6fa3c4f7956cbbb61303fc5
too_bad_ <toobaashfaque7@gmail.com>
2026-07-22 17:05:13 +0000

    datatypes file written

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+let name = "harry" // string
+let score = 8 // number
+let isLoggedIn = true //boolean
+let state; //undefined
+let window = null;
+console.log(typeof(window))
+console.log(typeof(state))
