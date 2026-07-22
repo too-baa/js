a280ca0558fb5c339dd14919b1dca16717e08097
too_bad_ <toobaashfaque7@gmail.com>
2026-07-22 17:29:23 +0000

    conversion

---
+++ 01_basics/03_conversionofdatatypes.js
@@ -0,0 +1,34 @@
+/*
+'76'=> 76, number
+'353abc'=> NaN, number
+'abc'=> NaN, number
+true=> 1; false=0, number
+unddefined=> NaN, number
+*/
+
+let score1 = "7"
+// console.log(typeof score1)
+
+// let valueInNumber = Number(score1)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let score2 = "65abc"
+// let valueInNumber = Number(score2)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let score3 = 'abh'
+// let valueInNumber = Number(score3)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let score4 = true
+// let valueInNumber = Number(score4)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+let score5 = undefined
+let valueInNumber = Number(score5)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
