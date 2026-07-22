e8eee07809814e8ffdc417365de90f91394c6682
too_bad_ <toobaashfaque7@gmail.com>
2026-07-22 17:08:40 +0000

    changes in datatypes file

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,5 +3,8 @@ let score = 8 // number
 let isLoggedIn = true //boolean
 let state; //undefined
 let window = null;
+
+/* you may use typeof(<>) or simply typeof <>
+*/
 console.log(typeof(window))
-console.log(typeof(state))
+console.log(typeof state)
