8154ca55a8c60c5313b345567bd0546565034e0e
too_bad_ <toobaashfaque7@gmail.com>
2026-07-22 18:11:28 +0000

    conversions completed

--- 01_basics/03_conversionofdatatypes.js
+++ 01_basics/03_conversionofdatatypes.js
@@ -1,4 +1,4 @@
-/*
+/***********************
 CONVERSION TO NUMBER
 '76'=> 76, number
 '353abc'=> NaN, number
@@ -8,14 +8,14 @@ undefined=> NaN, number
 null=> 0
 */
 
-// let score1 = "7"
+let score1 = "7"
 // console.log(typeof score1)
 
-// let valueInNumber = Number(score1)
+let valueInNumber = Number(score1)
 // console.log(typeof valueInNumber)
 // console.log(valueInNumber)
 
-/*
+/**************************
 CONVERSION TO BOOLEAN
 1 & other numbers=> true; 0=>false
 ''=> false
@@ -24,11 +24,11 @@ undefined=> false
 null=> false
 */
 
-// let isLoggedIn = null
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
+let isLoggedIn = null
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
 
-/*
+/***********************
 CONVERSION TO STRING
 45=> '45'
 true=> 'true'
@@ -36,6 +36,37 @@ undefined=> 'undefined'
 null=> 'null'
 */
 let str = null
-console.log(typeof str)
+// console.log(typeof str)
 let stringStr = String(str)
-console.log(stringStr)
+// console.log(stringStr)
+
+/************ OPERATIONS **************/
+let value = 9
+// console.log(-value)
+
+let value2 = 8
+// console.log(value+value2)
+// console.log(value*value2)
+// console.log(value/value2)
+// console.log(value%value2)
+// console.log(value**value2)
+
+let str1 = "hello"
+let str2 = "world"
+// console.log(str1+str2)
+// console.log(str1-str2)
+// console.log(str1/str2)
+
+// console.log("1" + 2)
+// console.log("1"+ 2 + 2)
+// console.log(1 + 2 + "2") 
+
+//bad practices
+// console.log(3 + 4 % 5 * 3)
+//console.log(-true); => -1
+// console.log(+""); =>0
+
+let num1 = num2 = num3 = 9 //valid
+num1+=1 //or num++
+console.log(num1);
+
